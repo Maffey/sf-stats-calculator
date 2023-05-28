3dac86cdd072009df4861d417b0e1a30d9ef97bb
Mateusz Kuniczuk <matiusz98@gmail.com>
2023-05-28 20:53:09 +0200

    Formatting, fix indentation in printer report

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,22 @@ fn main() {
 
     let primary_attribute: f64 = match primary_attribute.trim().parse() {
         Ok(value) => value,
-        Err(_) => panic!("Error while trying to parse the value!")
+        Err(_) => panic!("Error while trying to parse the value!"),
     };
 
     let constitution = (primary_attribute * 0.8) as u32;
     let luck = (primary_attribute * 0.4) as u32;
     let secondary_attributes = (primary_attribute * 0.1) as u32;
 
-    println!("Main attribute: {primary_attribute}
-    Recommended attributes are:
-    Constitution: {constitution}
-    Luck: {luck}
-    Secondary attributes: {secondary_attributes}
-    Have fun playing!
-    Press ENTER to close the app.");
+    println!(
+        "Main attribute: {primary_attribute}
+Recommended attributes are:
+Constitution: {constitution}
+Luck: {luck}
+Secondary attributes: {secondary_attributes}
+Have fun playing!
+Press ENTER to close the app."
+    );
 
     io::stdin().read_line(&mut String::new()).unwrap();
 }
